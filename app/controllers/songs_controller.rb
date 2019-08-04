3a26945bc7cddf2e2936d9c03d478ccc5ab3bc48
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-04 20:56:50 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,13 +17,13 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.create(song_params(:name, :bio))
+    @song = Song.create(song_params(:name, :artist_id, :genre_id))
     redirect_to song_path(@song)
   end
 
   def update
     @song = Song.find(params[:id])
-    @song.update(song_params(:name, :bio))
+    @song.update(song_params(:name, :artist_id, :genre_id))
     redirect_to song_path(@song)
   end
 
