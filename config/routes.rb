23b44ed79d78546d6668bc75e5caa2ce62ead7a1
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-04 20:40:05 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :accounts
+  resources :artists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
